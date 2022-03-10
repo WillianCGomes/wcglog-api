bec2bf2fe4f72d3d7fb477161b24c9535c15d378
Willian Carlos Gomes <gomes.cwillian@gmail.com>
2022-03-10 01:34:38 -0300

    Configuração JPA

--- Workspace STS/wcglog-api/src/main/java/com/wcg/wcglog/api/controller/ClienteController.java
+++ Workspace STS/wcglog-api/src/main/java/com/wcg/wcglog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.wcg.wcglog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +13,14 @@ import com.wcg.wcglog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext // Ingetando um EntityManager na variável
+	private EntityManager manager;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João2");
-		cliente1.setTelefone("46992342433");
-		cliente1.setEmail("joao.cliente@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("46992342434");
-		cliente2.setEmail("maria.cliente@gmail.com");
-			
-		
-		return Arrays.asList(cliente1, cliente2);
+
+		// Pegando todos os objetos de Cliente e retornando como lista
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
-	
+
 }
--- Workspace STS/wcglog-api/src/main/java/com/wcg/wcglog/domain/model/Cliente.java
+++ Workspace STS/wcglog-api/src/main/java/com/wcg/wcglog/domain/model/Cliente.java
@@ -1,15 +1,33 @@
 package com.wcg.wcglog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity // Definindo que a classe Cliente representa uma Entidade
+@Table(name = "cliente") // Nome da tabela no banco de dados
 public class Cliente {
 
+	@EqualsAndHashCode.Include // EqualsAndHashCode apenas para id
+	@Id // Chave Primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Banco de dados controla a estrategia
 	private Long id;
+
+	
 	private String nome;
 	private String email;
+
+	@Column(name = "fone") // Especifica o nome da coluna no banco de dados
 	private String telefone;
 
 }
