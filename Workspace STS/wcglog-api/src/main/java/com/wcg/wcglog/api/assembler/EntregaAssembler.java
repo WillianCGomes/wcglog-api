879409256d9e8f0bb11ea9114eaca95bdf7a1942
Willian Carlos Gomes <gomes.cwillian@gmail.com>
2022-03-26 01:46:27 -0300

    Simplificando a transformação de objetos com ModelMapper

---
+++ Workspace STS/wcglog-api/src/main/java/com/wcg/wcglog/api/assembler/EntregaAssembler.java
@@ -0,0 +1,28 @@
+package com.wcg.wcglog.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.wcg.wcglog.api.model.EntregaModel;
+import com.wcg.wcglog.domain.model.Entrega;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Component
+public class EntregaAssembler {
+
+	private ModelMapper modelMapper;
+
+	public EntregaModel toModel(Entrega entrega) {
+		return modelMapper.map(entrega, EntregaModel.class);
+	}
+	
+	public List<EntregaModel> toCollectionModel(List<Entrega> entregas)
+	{
+		return entregas.stream().map(this::toModel).collect(Collectors.toList());
+	}
+} 
--- Workspace STS/wcglog-api/src/main/java/com/wcg/wcglog/api/controller/EntregaController.java
+++ Workspace STS/wcglog-api/src/main/java/com/wcg/wcglog/api/controller/EntregaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wcg.wcglog.api.model.DestinatarioModel;
+import com.wcg.wcglog.api.assembler.EntregaAssembler;
 import com.wcg.wcglog.api.model.EntregaModel;
 import com.wcg.wcglog.domain.model.Entrega;
 import com.wcg.wcglog.domain.repository.EntregaRepository;
@@ -29,37 +29,24 @@ public class EntregaController {
 
 	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitacaoEntregaService;
+	private EntregaAssembler entregaAssembler;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
-		return solicitacaoEntregaService.solicitar(entrega);
+	public EntregaModel solicitar(@Valid @RequestBody Entrega entrega) {
+		return entregaAssembler.toModel(solicitacaoEntregaService.solicitar(entrega));
 	}
 
 	@GetMapping
-	public List<Entrega> listar() {
-		return entregaRepository.findAll();
+	public List<EntregaModel> listar() {
+		return entregaAssembler.toCollectionModel(entregaRepository.findAll());
 	}
 
 	@GetMapping("/{entregaId}")
 	public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId) {
-		return entregaRepository.findById(entregaId).map(entrega -> {
-			EntregaModel entregaModel = new EntregaModel();
-			entregaModel.setId(entrega.getId());
-			entregaModel.setNomeCliente(entrega.getCliente().getNome());
-			entregaModel.setDestinatario(new DestinatarioModel());
-			entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
-			entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
-			entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
-			entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
-			entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
-			entregaModel.setTaxa(entrega.getTaxa());
-			entregaModel.setStatus(entrega.getStatus());
-			entregaModel.setDataPedido(entrega.getDataPedido());
-			entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
-
-			return ResponseEntity.ok(entregaModel);
-		}).orElse(ResponseEntity.notFound().build());
+		return entregaRepository.findById(entregaId)
+				.map(entrega -> ResponseEntity.ok(entregaAssembler.toModel(entrega)))
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
